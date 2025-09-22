62a3246dd7998b62414399099fc537dc6da61174
robertJr18 <roberteloy@Robertpc.localdomain>
2025-09-22 15:10:35 -0500

    modelsPart1

--- src/main/java/com/unimag/gestion_vuelos_reservas/models/Airline.java
+++ src/main/java/com/unimag/gestion_vuelos_reservas/models/Airline.java
@@ -1,4 +1,30 @@
 package com.unimag.gestion_vuelos_reservas.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "airlines")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Airline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String code;
+    private String name;
+
+    @OneToMany(mappedBy = "flights")
+    @Builder.Default
+    private List<Flight> flights =  new ArrayList<>();
+
+    public void addFlight(Flight f) {
+        flights.add(f);
+        f.setAirline(this);
+    }
 }
--- src/main/java/com/unimag/gestion_vuelos_reservas/models/Tag.java
+++ src/main/java/com/unimag/gestion_vuelos_reservas/models/Tag.java
@@ -1,4 +1,29 @@
 package com.unimag.gestion_vuelos_reservas.models;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    @Builder.Default
+    private Set<Flight> flights = new HashSet<>();
+
+    public void addFlight(Flight flight) {
+        flights.add(flight);
+        flight.getTags().add(this);
+    }
 }
