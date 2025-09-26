623a43efc0478ae9e916b4c593c3b5f58abb2704
robertJr18 <roberteloy@Robertpc.localdomain>
2025-09-26 15:59:51 -0500

    arreglo AirportDtos

--- src/main/java/com/unimag/gestion_vuelos_reservas/api/dto/AirportDtos.java
+++ src/main/java/com/unimag/gestion_vuelos_reservas/api/dto/AirportDtos.java
@@ -8,6 +8,6 @@ import java.util.List;
 public class AirportDtos {
     public record AirportCreateRequest(String code, String name, String city) implements Serializable {}
     public record AirportUpdateRequest(String code, String name, String city) implements Serializable {}
-    public record AirportResponse(Long id,String code, String name, String city, List<FlightDtos.FlightResponse> departures,List<FlightDtos.FlightResponse> arrivals) implements Serializable {}
+    public record AirportResponse(Long id,String code, String name, String city) implements Serializable {}
 }
 
