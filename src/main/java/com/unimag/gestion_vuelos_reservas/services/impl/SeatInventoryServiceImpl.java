c37728588cabcaf3be36de0dfebec965a447ccdd
robertJr18 <roberteloy@Robertpc.localdomain>
2025-09-30 14:07:45 -0500

    fix(services): Cambia nombre de SeatInventoryImpl a SeatInventoryServiceImpl

--- src/main/java/com/unimag/gestion_vuelos_reservas/services/impl/SeatInventoryServiceImpl.java
+++ src/main/java/com/unimag/gestion_vuelos_reservas/services/impl/SeatInventoryServiceImpl.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class SeatInventoryImpl implements SeatInventoryService {
+public class SeatInventoryServiceImpl implements SeatInventoryService {
 
     private final SeatInventoryRepository seatInventoryRepository;
     private final FlightRepository flightRepository;
---
+++ src/test/java/com/unimag/gestion_vuelos_reservas/services/Impl/AirlineServiceImplTest.java
@@ -0,0 +1,4 @@
+package com.unimag.gestion_vuelos_reservas.services.Impl;
+
+public class AirlineServiceImplTest {
+}
