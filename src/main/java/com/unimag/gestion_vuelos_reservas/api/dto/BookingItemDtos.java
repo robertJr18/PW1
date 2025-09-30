e90c7f9af8ac6087297de7ca7af5fc53a4db2dbe
Jose Alejandro <214848445+JoseRodriguez0001@users.noreply.github.com>
2025-09-29 19:40:27 -0500

    chore(dtos): Booking, Item, Passenger

--- src/main/java/com/unimag/gestion_vuelos_reservas/api/dto/BookingItemDtos.java
+++ src/main/java/com/unimag/gestion_vuelos_reservas/api/dto/BookingItemDtos.java
@@ -10,6 +10,6 @@ import java.time.OffsetDateTime;
 
 public class BookingItemDtos {
     public record BookingItemCreateRequest(BigDecimal price, Integer segmentOrder, Cabin cabin, Long flighId) implements Serializable {}
-    public record BookingItemUpdateRequest(Long id, BigDecimal price, Integer segmentOrder, Cabin cabin,@Nullable Long flight_id ) implements Serializable {}
+    public record BookingItemUpdateRequest(BigDecimal price, Integer segmentOrder, Cabin cabin,@Nullable Long flight_id ) implements Serializable {}
     public record BookingItemResponse(Long id, BigDecimal price, Integer segmentOrder, Cabin cabin, FlightDtos.FlightResponse flightDto) implements Serializable {}
 }
