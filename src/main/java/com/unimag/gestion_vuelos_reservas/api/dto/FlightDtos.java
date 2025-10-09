11a42ab029d1f5853ba4a59180de9232b915e7a5
robertJr18 <roberteloy@Robertpc.localdomain>
2025-10-09 17:42:30 -0500

    fix(Dtos): agrega validaciones a los Dto's

--- src/main/java/com/unimag/gestion_vuelos_reservas/api/dto/FlightDtos.java
+++ src/main/java/com/unimag/gestion_vuelos_reservas/api/dto/FlightDtos.java
@@ -1,5 +1,8 @@
 package com.unimag.gestion_vuelos_reservas.api.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+
 import java.io.Serializable;
 import java.time.OffsetDateTime;
 import java.util.Set;
@@ -11,8 +14,8 @@ public class FlightDtos {
     public record TagRef(Long id, String name) implements Serializable {}
 
 
-    public record FlightCreateRequest(String number, OffsetDateTime departureTime, OffsetDateTime arrivalTime,
-                                      Long airlineId,Long originId, Long destinationId, Set<Long> tagIds)implements Serializable {}
+    public record FlightCreateRequest(@NotBlank @Max(7) String number, @NotBlank OffsetDateTime departureTime, @NotBlank OffsetDateTime arrivalTime,
+                                      @NotBlank Long airlineId, @NotBlank Long originId, @NotBlank Long destinationId, @NotBlank Set<Long> tagIds)implements Serializable {}
 
     public record FlightUpdateRequest(String number, OffsetDateTime departureTime, OffsetDateTime arrivalTime,
                                       Long airlineId,Long originId, Long destinationId, Set<Long> tagIds)implements Serializable {}
--- src/main/java/com/unimag/gestion_vuelos_reservas/api/dto/SeatInventoryDtos.java
+++ src/main/java/com/unimag/gestion_vuelos_reservas/api/dto/SeatInventoryDtos.java
@@ -1,11 +1,13 @@
 package com.unimag.gestion_vuelos_reservas.api.dto;
 
 import com.unimag.gestion_vuelos_reservas.models.Cabin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 import java.io.Serializable;
 
 public class SeatInventoryDtos {
-    public record SeatInventoryCreateRequest(Cabin cabin, Integer totalSeats, Integer availableSeats,Long flightId) implements Serializable {}
+    public record SeatInventoryCreateRequest(@NotBlank Cabin cabin, @Min(1) Integer totalSeats, @Min(0) Integer availableSeats, @NotBlank Long flightId) implements Serializable {}
     public record SeatInventoryUpdateRequest(Cabin cabin, Integer totalSeats, Integer availableSeats, Long flightId) implements Serializable {}
     public record SeatInventoryResponse(Long id, Cabin cabin, Integer totalSeats, Integer availableSeats, FlightDtos.FlightResponse flight) implements Serializable {}
 }
